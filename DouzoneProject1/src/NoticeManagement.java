90b5fd7c8ab489c8ea717b5d7860127f281826d5
LJH <hosboy93@naver.com>
2023-04-12 18:54:02 +0900

    feat : 공고클래스 메소드 추가

---
+++ DouzoneProject1/src/NoticeManagement.java
@@ -0,0 +1,33 @@
+
+public class NoticeManagement {
+	
+	// 공고 상태 변경
+	private void noticeStatusChange() {
+		
+	}
+	
+	// 공고 삭제
+	private void noticeDeletion() {
+		
+	}
+	
+	// 공고 수정
+	private void noticeModification() {
+		
+	}
+	
+	// 공고 등록
+	private void noticeRegistration() {
+		
+	}
+	
+	// 공고 목록 확인
+	private void noticeListCheck() {
+		
+	}
+	
+	// 공고 지원
+	private void noticeApplication() {
+		
+	}
+}
