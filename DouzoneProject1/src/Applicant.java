4bc8261161958b8e67d51e68e9bb35575c519795
LJH <hosboy93@naver.com>
2023-04-13 23:25:26 +0900

    refactor : 라인 수정

--- DouzoneProject1/src/Applicant.java
+++ DouzoneProject1/src/Applicant.java
@@ -9,39 +9,42 @@ public class Applicant implements Serializable {
 	private boolean loginStatus;
 	public List<Career> clist = new ArrayList<>(); 
 	
-	public String getmemberID() {
-		return memberID;
-	}
-	public void setmemberID(String memberID) {
-		this.memberID = memberID;
-	}
-	public String getmemberPW() {
-		return memberPW;
-	}
-	public void setmemberPW(String memberPW) {
-		this.memberPW = memberPW;
-	}
-	public String getuserName() {
-		return userName;
-	}
-	public void setuserName(String userName) {
-		this.userName = userName;
-	}
-	public boolean isLoginStatus() {
-		return loginStatus;
-	}
-	public void setLoginStatus(boolean loginStatus) {
-		this.loginStatus = loginStatus;
-	}
-	
-	
-	
-	
 	public Applicant(String memberID, String memberPW, String userName) {
 		super();
 		this.memberID = memberID;
 		this.memberPW = memberPW;
 		this.userName = userName;
+	}	
+	
+	public String getmemberID() {
+		return memberID;
 	}
 	
+	public void setmemberID(String memberID) {
+		this.memberID = memberID;
+	}
+	
+	public String getmemberPW() {
+		return memberPW;
+	}
+	
+	public void setmemberPW(String memberPW) {
+		this.memberPW = memberPW;
+	}
+	
+	public String getuserName() {
+		return userName;
+	}
+	
+	public void setuserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public boolean isLoginStatus() {
+		return loginStatus;
+	}
+	
+	public void setLoginStatus(boolean loginStatus) {
+		this.loginStatus = loginStatus;
+	}
 }
